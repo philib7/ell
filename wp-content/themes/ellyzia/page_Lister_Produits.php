32a294ffdb33217d39f7412fe10f6226ab5c58a8
philib7 <philippe.boin@laposte.net>
2021-04-27 10:08:58 +0200

    2eme modification page_lister_produits.php

--- wp-content/themes/ellyzia/page_Lister_Produits.php
+++ wp-content/themes/ellyzia/page_Lister_Produits.php
@@ -5,8 +5,9 @@ if ( !defined('ABSPATH')) exit;
  */
 
 $user_info = is_user_logged_in();
-
-
+$fields = get_fields(get_the_id());
+$cloud1 = get_template_directory_uri().'/BuildFrontAsset/dist/images/bigcloud.png';
+$cloud2 = get_template_directory_uri().'/BuildFrontAsset/dist/images/bigcloud2.png';
 if (!$user_info)
 {
     wp_redirect( get_home_url());
@@ -20,12 +21,23 @@ $products = ell_get_products();
 ?>
 
 <?php
-get_header(); ?>
-
+get_header('admin'); ?>
+    <img src="<?php echo $cloud1; ?>" alt="" class="absolute cloud1">
+    <article class="background-admin py-16">
     <section>
-
+        <div class="flex justify-center">
+            <button name="edit_new_product" value="true" class="mt-2 bg-teal-500 p-4 flex items-center justify-center text-white border">Nouveau Produit</button>
+            <button name="save_edit_product" value="true" class="mt-2 bg-teal-500 p-4 flex items-center justify-center text-white border">Enregistrer</button>
+            <button type=reset" name="annule_edit_product" value="true" class="mt-2 bg-teal-500 p-4 flex items-center justify-center text-white border">Annuler</button>
+        </div>
         <div class="container">
-
+            <div class="flex items-center ">
+                <!-- Image debut -->
+                <div class="w-1/2 flex justify-center">
+                    <img src="<?php echo $fields['gallery']['url']; ?>" width="150px" alt="">
+                </div
+            </div>
+                <!-- Image fin -->
             <ul>
                 <?php
                 // on récupère l'ID de chaque produit
@@ -48,7 +60,10 @@ get_header(); ?>
                 ?>
             </ul>
         </div>
+
     </section>
+    </article>
+    <img src="<?php echo $cloud2; ?>" alt="" class="absolute cloud2">
 
 <?php
 get_footer();
