b57405d3219625c2300fc76aaa4b198bca561a05
philib7 <philippe.boin@laposte.net>
2021-04-28 17:59:32 +0200

    updat designation page produit + début image

--- wp-content/themes/ellyzia/hooks-end/controller/product.php
+++ wp-content/themes/ellyzia/hooks-end/controller/product.php
@@ -28,8 +28,13 @@ function add_product() {
 		$matiere = $_POST['matiere-product'];
         $pierre = $_POST['pierre-product'];
         $taille = $_POST['taille-product'];
-
-		// Insert du nouveau produit
+        $description = $_POST['description-product'];
+        $stock = $_POST['stock-product'];
+        $prixht = $_POST['prixht-product'];
+        $image = $_FILES['image-product'];
+var_dump($image);
+die();
+        // Insert du nouveau produit
 		// TODO Déplacer le code dans le modal et créer une function
 		// Create post object
 		$product = array(
@@ -44,11 +49,23 @@ function add_product() {
 		// Insert the post into the database
 		$id_product = wp_insert_post( $product );
 
+		//ajout image
+        $upload_id = wp_insert_attachment( array(
+            'guid'           => $new_file_path,
+            'post_mime_type' => $new_file_mime,
+            'post_title'     => preg_replace( '/\.[^.]+$/', '', $profilepicture['name'] ),
+            'post_content'   => '',
+            'post_status'    => 'inherit'
+        ), $new_file_path );
+
 		// Avec l'id editer les champs du produit
 		ell_set_type_byProduct_id($id_product, $type);
 		ell_set_reference_byProduct_id($id_product, $reference);
 		ell_set_matiere_byProduct_id($id_product, $matiere);
-        ell_set_pierre_byProduct_id($id_product,  $pierre);
-        ell_set_taille_byProduct_id($id_product,  $taille);
+        ell_set_pierre_byProduct_id($id_product, $pierre);
+        ell_set_taille_byProduct_id($id_product, $taille);
+        ell_set_description_byProduct_id($id_product, $description);
+        ell_set_stock_byProduct_id($id_product, $stock);
+        ell_set_prixht_byProduct_id($id_product, $prixht);
 	}
 }
