97340433f078d0fa11bcd320654e181efb4991e0
Quentin Michinot <quentin.michinot@live.fr>
2021-04-28 22:40:21 +0200

    add multiple

--- wp-content/themes/ellyzia/page-home.php
+++ wp-content/themes/ellyzia/page-home.php
@@ -82,6 +82,67 @@ get_header(); ?>
                     </div>
                 </div>
             </a>
+
+            <!-- La Maison Bloc -->
+            <a class="flex flex-col lg:flex-row items-center justify-between bg-EllGrayVeryLight card w-full mt-6 md:mt-16" href="#">
+                <div class="text-center text-EllGrayLight lg:w-1/2 w-full flex flex-col items-center justify-center mt-8 px-4 lg:pt-0">
+                    <div class="uppercase font-montserrat">Collection</div>
+                    <div class="lg:text-7xl text-4xl">Délinéation</div>
+
+                    <div class="mt-6 uppercase lg:overflow-hidden text-center">
+                        <div class="text-sm lg:text-base card-discover font-montserrat tracking-widest">
+                            Découvrir la collection
+                        </div>
+                    </div>
+                </div>
+
+                <div class="w-full lg:w-1/2">
+                    <img src="<?php echo $homeCollection; ?>" class="w-full" alt="">
+                </div>
+            </a>
+
+            <!-- Left -->
+            <a class="bg-EllGrayVeryLight md:w-47 w-full flex justify-center card card-min mt-6 md:mt-16" href="#">
+                <div style="" class="w-full bg-center pb-6 relative">
+                    <div class="">
+                        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/BuildFrontAsset/dist/images/photo-1454117096348-e4abbeba002c.png" alt="">
+                    </div>
+
+                    <div class="text-EllGrayLight mt-8 lg:mt-16 absolute top-0 w-full">
+                        <div class="flex flex-col items-center">
+                            <div class="uppercase font-montserrat">Collection</div>
+                            <div class="lg:text-7xl text-4xl">Ondes</div>
+                        </div>
+
+                        <div class="mt-6 uppercase lg:overflow-hidden text-center">
+                            <div class="text-sm lg:text-base mt-4 lg:mt-0 card-discover font-montserrat tracking-widest">
+                                Découvrir la collection
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a class="bg-EllGrayVeryLight md:w-47 w-full flex justify-center card card-min mt-6 md:mt-16" href="#">
+                <div style="" class="w-full bg-center pb-6 relative">
+                    <div class="">
+                        <img class="w-full" src="<?php echo get_template_directory_uri(); ?>/BuildFrontAsset/dist/images/summer.png" alt="">
+                    </div>
+
+                    <div class="text-EllGrayLight mt-8 lg:mt-16 absolute top-0 w-full">
+                        <div class="flex flex-col items-center">
+                            <div class="uppercase font-montserrat">Collection</div>
+                            <div class="lg:text-7xl text-4xl">Cap</div>
+                        </div>
+
+                        <div class="mt-6 uppercase lg:overflow-hidden text-center">
+                            <div class="text-sm lg:text-base mt-4 lg:mt-0 card-discover font-montserrat tracking-widest">
+                                Découvrir la collection
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
         </div>
 
         <!-- La Maison Bloc -->
